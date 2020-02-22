5e428629c74e6d25f2385a744417bc215105dbba
nivleM-ed <melvinedward98@gmail.com>
2020-02-22 22:58:08 +0800

    Enhance SP

--- database/F_RECIPE_DELETE.sql
+++ database/F_RECIPE_DELETE.sql
@@ -3,15 +3,23 @@ CREATE OR REPLACE FUNCTION public.F_RECIPE_DELETE(
 	IN in_id VARCHAR
 )
     RETURNS TABLE(
-	done INTEGER
+	done INTEGER,
+	remove_file VARCHAR
 	)
     LANGUAGE 'plpgsql'
 AS $$
+DECLARE
+remove_file VARCHAR;
 
 BEGIN
+	SELECT recipe.pic_path INTO remove_file
+	FROM public."recipe" AS recipe
+	WHERE recipe.id = in_id; 
+	
 	DELETE FROM public."recipe" 
 	WHERE public."recipe".id = in_id;
+	
 	RETURN QUERY
-		SELECT 1 AS done;
+		SELECT 1 AS done, remove_file;
 END;
 $$;
